18107721b612845536d5df23737f9c1efee20b8d
Irene de Nicolo <irene.denicolo89@gmail.com>
2018-04-21 11:39:54 +0200

    connected to typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
 import Controller from "./controller"
+import setupDb from './db'
+
 
 const port = process.env.PORT || 4000
 
@@ -8,4 +10,8 @@ const app = createKoaServer({
    controllers: [Controller]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
